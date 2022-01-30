63f799a35663aef357230abb77fa1df1f694e879
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2022-01-30 20:25:53 +0330

    isEventOrOdd-function

---
+++ js.js
@@ -0,0 +1,12 @@
+let x = +(prompt('enter your number'));
+
+function isEventOrOdd (){
+    if (x % 2 == 0){
+        alert("is event")
+    } else {
+        alert("is odd")
+    }
+}
+
+isEventOrOdd()
+
